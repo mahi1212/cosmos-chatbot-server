434b6285af09a2c31aff790596be052ff92efb45
mahi1212 <mahi121.mr@gmail.com>
2024-05-29 05:23:47 +0600

    delete and get route added to chat

--- src/controllers/chat-controllers.ts
+++ src/controllers/chat-controllers.ts
@@ -76,3 +76,68 @@ export const generateChatCompletion = async (req: Request, res: Response, next: 
         });
     }
 };
+
+
+
+export const getAllChats = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const user = await User.findById(res.locals.jwtData.id)
+
+        if (!user) {
+            return res.status(400).json({
+                message: "User not registered or TOKEN malfuncationed"
+            })
+        }
+
+        // console.log(user._id.toString(), res.locals.jwtData.id)
+
+        if(user._id.toString() !== res.locals.jwtData.id) {
+            return res.status(401).json({
+                message: "Permission denied",
+            })
+        }
+
+        return res.status(200).json({
+            message: "OK",
+            chats: user.chats
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "ERROR",
+            reason: error.message
+        })
+    }
+}
+
+
+export const deleteAllChats = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const user = await User.findById(res.locals.jwtData.id)
+
+        if (!user) {
+            return res.status(400).json({
+                message: "User not registered or TOKEN malfuncationed"
+            })
+        }
+
+        if(user._id.toString() !== res.locals.jwtData.id) {
+            return res.status(401).json({
+                message: "Permission denied",
+            })
+        }
+
+        // @ts-ignore
+        user.chats = []
+        await user.save()
+        return res.status(200).json({
+            message: "OK",
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "ERROR",
+            reason: error.message
+        })
+    }
+}
--- src/routes/chat-routes.ts
+++ src/routes/chat-routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { verifyToken } from "../utils/token-manager";
 import { chatCompletionValidator } from "../utils/validators";
-import { generateChatCompletion } from "../controllers/chat-controllers";
+import { deleteAllChats, generateChatCompletion, getAllChats } from "../controllers/chat-controllers";
 
 const chatRoutes = Router();
 
 chatRoutes.post("/new", chatCompletionValidator, verifyToken, generateChatCompletion)
+chatRoutes.get("/get-all", verifyToken, getAllChats)
+chatRoutes.delete("/delete-chat", verifyToken, deleteAllChats)
 
 export default chatRoutes;
