50474b7d062f236639e2a33fc838f1b50cc5add7
mahi1212 <mahi121.mr@gmail.com>
2024-08-31 14:58:46 +0600

    translation controller changed

--- src/routes/translate-routes.ts
+++ src/routes/translate-routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { loginValidator, validate } from "../utils/validators";
 import { makeTranslation } from "../controllers/translate-controllers";
+import { verifyToken } from "../utils/token-manager";
 
 const translationRoutes = Router();
 
 // validate(loginValidator),
-translationRoutes.post('/translate-rewrite', makeTranslation)
+translationRoutes.post('/translate-rewrite', verifyToken, makeTranslation)
 
 export default translationRoutes;
