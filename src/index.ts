7d14cd1bc7ddd11c6c8f468faf6f3b09a6f739ae
mahi1212 <mahi121.mr@gmail.com>
2024-09-03 01:09:53 +0600

    helmet added

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,8 @@ require('dotenv').config();
 
 const app = express()
 const port = process.env.PORT || 5000
+const helmet = require('helmet');
+app.use(helmet());
 
 // middlewares
 app.use(express.json())
